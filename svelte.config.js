d5ceb5b270f3ed8d2c1ef5216d333fd5e74315ff
dajxyz <github@jyllikoski.fi>
2025-02-26 16:20:02 +0200

    try4

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -27,4 +29,21 @@ const config = {
 	}
 };
 
-export default config;
+export default {
+	kit: {
+		adapter: adapter({
+			// See below for an explanation of these options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			},
+			platformProxy: {
+				configPath: 'wrangler.toml',
+				environment: undefined,
+				experimentalJsonConfig: false,
+				persist: false
+			}
+		})
+	}
+};
+
